b80dbfee70c2bfed87b22508c89d0e746fbeed87
Maham Kanwal <kanwalmahsm@gmail.com>
2025-09-17 23:41:14 -0700

    added js

---
+++ app.js
@@ -0,0 +1,22 @@
+const fullName = document.getElementById("fullName");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+
+
+if(fullName == " "){
+   document.write("name field is empty");
+}
+
+
+if(email.includes("@") || email.includes(".")){
+
+}
+
+if(password.length < 6){
+
+}
+
+if(password == confirmPassword){
+    
+}
