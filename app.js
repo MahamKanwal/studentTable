f6d50fbeeb0f9283bc53052ee7095f4e7e0dda47
Maham Kanwal <kanwalmahsm@gmail.com>
2025-09-22 07:33:05 -0700

    added js

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
+// const name = document.getElementById("name");
+// const email = document.getElementById("email");
+// const password = document.getElementById("password");
+// const confirmPassword = document.getElementById("confirmPassword");
 const form = document.getElementById("form");
-const inputs = form.querySelectorAll("input"); 
+const inputs = form.querySelectorAll("input");
 const state = {
   values: { name: "", email: "", password: "", confirmPassword: "" },
   errors: {},
 };
 
-for(let i of inputs){
-i.addEventListener("blur",()=>{
+for (let i of inputs) {
+  i.addEventListener("blur", () => {
     hideError(i.id);
   });
-  i.addEventListener("focus",()=>{
+  i.addEventListener("focus", () => {
     showError(i.id, state.errors[i.id]);
   });
-};
+}
 
 const rules = {
   name: v => (!v.trim() || v.length < 3 ? "name must be 3 characters" : ""),
   email: v => (!v.trim() || !v.includes("@") ? "enter a valid email" : ""),
-  password: v => (!v.trim() || v.length < 6 ? "password must be 6 characters" : ""),
-  confirmPassword: v => (!v.trim() || !(v == state.values.password) ? "Same as password" : ""),
+  password: v => !v.trim() || v.length < 6 ? "password must be 6 characters" : "",
+  confirmPassword: v => !v.trim() || !(v == state.values.password) ? "Same as password" : "",
 };
 
-const validateField = (id, value) =>{
- const errorMsg = rules[id](value);
- state.values[id] = value;
+const validateField = (id, value) => {
+  const errorMsg = rules[id](value);
+  state.values[id] = value;
   state.errors[id] = errorMsg;
-   showError(id, errorMsg);
-   return errorMsg;
-}
+  showError(id, errorMsg);
+  return errorMsg;
+};
 
 const formHandler = (e) => {
   const { id, value } = e.target;
@@ -38,23 +42,21 @@ const hideError = (id) => {
   document.querySelector(`div[data-error=${id}]`).innerHTML = "";
 };
 
-const showError = (id, message="") => {
+const showError = (id, message = "") => {
   document.querySelector(`div[data-error=${id}]`).innerHTML = message;
 };
 
 const handleSubmit = (e) => {
-e.preventDefault();
-let hasError = false;
-let entries = Object.entries(state.values);
-for(let e of entries){
-let error = validateField(e[0], e[1]);
-if(error) hasError = true;
-}
-if(!hasError) console.log(state.values);
-
-}
+  e.preventDefault();
+  let hasError = false;
+  let entries = Object.entries(state.values);
+  for (let e of entries) {
+    let error = validateField(e[0], e[1]);
+    if (error) hasError = true;
+  }
+  if (!hasError) console.log(state.values);
+};
 
 form.addEventListener("submit", handleSubmit);
 form.addEventListener("input", formHandler);
-
 
